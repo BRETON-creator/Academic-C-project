00a018b019a50739da15edf0b5477b3f2a2c5221
paul <paul.breton@grenoble-inp.org>
2024-05-21 17:49:38 +0200

    encore et toujours des listes chainées

--- implem/ei_implementation_entry.c
+++ implem/ei_implementation_entry.c
@@ -155,6 +155,37 @@ bool ei_callback_entry(ei_widget_t		widget, struct ei_event_t*	event, ei_user_pa
     }
 }
 
+// Fonction pour convertir la liste chaînée de lettre en texte (chaîne de caractères) pour utilier ensuite la fonction hw_text_create_surface
+const char* from_list_to_string(letter* tab) {
+    int length = 0;
+    letter* current = tab;
+    while (current != NULL) {
+        length++;
+        current = current->next;
+    }
+    char* str = (char*)malloc((length + 1) * sizeof(char));
+    current = tab;
+    for (int i = 0; i < length; i++) {
+        str[i] = current->data;
+        current = current->next;
+    }
+    str[length] = '\0';
+    return str;
+}
+
+// Fonction pour convertir une chaîne de caractères en liste chaînée
+letter* from_string_to_List(const char* str) {
+    letter* tab = create_letter(str[0]);
+    letter* current = tab;
+
+    for (int i = 1; str[i] != '\0'; i++) {
+        current->next = create_letter(str[i]);
+        current = current->next;
+    }
+    return tab;
+}
+
+
 /**
  * @brief	Sets the text displayed in the entry widget.
  *
